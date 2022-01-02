a303ddf2c5cfb4e471ff9c5166983427e90cd842
github usernameIfeAkiWad <ifeoluwa.wade@gmail.com>
2022-01-02 00:54:55 -0600

    solved reverse string challenge

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,25 +1,27 @@
-//
-
+// will be reverising this string without built-in reverse method.
+//using iteration
 function reverseString(str) {
-  let splitStr = str.split('')
-  let reversed = []
-
-  for (let i = 0; i < str.length; i++) {
-    reversed.push(splitStr.pop())
-  }
-
+  // declaring variable reversed and initializing it with empty string
+  let reversed = '';
+  // using for loop to iterate through string
+    for (const c of str) {
+      // appending empty string on to c and the setting it to varaible reversed
+        reversed = c + reversed;
+    }
+    //returning reversed outside of loop
+    return reversed;
 
 }
 
 if (require.main === module) {
   // add your own tests in here
-  console.log("Expecting: 'ih'");
-  console.log("=>", reverseString("hi"));
+  console.log("Expecting: 'hi'");
+  console.log("=>", reverseString("ih"));
 
   console.log("");
 
-  console.log("Expecting: 'ybabtac'");
-  console.log("=>", reverseString("catbaby"));
+  console.log("Expecting: 'catbaby'");
+  console.log("=>", reverseString("ybabtac"));
 }
 
 module.exports = reverseString;
